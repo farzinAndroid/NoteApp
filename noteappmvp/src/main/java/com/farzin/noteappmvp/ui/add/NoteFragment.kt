8c02e02e54d42d7124764313d06c860a0df42f28
Farzin <farzinallahyari1234@gmail.com>
2025-11-01 13:52:04 +0330

    develop Note page ui

---
+++ noteappmvp/src/main/java/com/farzin/noteappmvp/ui/add/NoteFragment.kt
@@ -0,0 +1,34 @@
+package com.farzin.noteappmvp.ui.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.farzin.noteappmvp.R
+import com.farzin.noteappmvp.databinding.FragmentNoteBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class NoteFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentNoteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentNoteBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // init views
+        binding.apply {
+
+        }
+    }
+
+}
