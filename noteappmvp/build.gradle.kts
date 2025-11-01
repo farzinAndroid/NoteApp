c3e6334d5e8aa39db346c9ade600f90b69400073
Farzin <farzinallahyari1234@gmail.com>
2025-11-01 11:10:08 +0330

    Initial commit

---
+++ noteappmvp/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id ("com.google.dagger.hilt.android")
+    alias(libs.plugins.devtools.ksp)
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.farzin.noteappmvp"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //hilt di
+    implementation (libs.hilt.android)
+    ksp (libs.hilt.compiler)
+
+
+    //Room
+    implementation (libs.androidx.room.runtime)
+    implementation (libs.androidx.room.ktx)
+    ksp (libs.androidx.room.compiler)
+
+    //Nav
+    implementation(libs.androidx.navigation.fragment.ktx.v295)
+    implementation(libs.androidx.navigation.ui.ktx.v295)
+
+
+    //retrofit
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation(libs.logging.interceptor)
+    implementation(libs.okhttp)
+    implementation(libs.gson)
+
+    //coil
+    implementation(libs.coil)
+
+    //rxjava for retrofit
+    implementation(libs.adapter.rxjava3)
+
+
+    //Rx java
+    implementation (libs.rxandroid)
+    // Because RxAndroid releases are few and far between, it is recommended you also
+    // explicitly depend on RxJava's latest version for bug fixes and new features.
+    // (see https://github.com/ReactiveX/RxJava/releases for latest 3.x.x version)
+    implementation (libs.rxjava)
+    implementation(libs.rxjava3.extensions)
+    implementation (libs.rxbinding)
+}
